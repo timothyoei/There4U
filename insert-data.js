979ccc6e6b7ead5a7ba0080af3b331554da47827
mlin2024 <mlin2024@gmail.com>
2021-09-25 16:53:09 +0100

    Added functionality to read stories from the database by tag

--- insert-data.js
+++ insert-data.js
@@ -2,61 +2,79 @@ const { MongoClient } = require("mongodb");
 const url = "mongodb+srv://egg:egg@cluster0.gsu6g.mongodb.net/legendary_waffle_database?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
 const client = new MongoClient(url);
 const dbName = "legendary_waffle_database";
+const db = client.db(dbName);
+// Use the collection "stories"
+const col = db.collection("stories");
 
- async function run() {
+async function connect() {
 
     try {
 
-         await client.connect();
+        await client.connect();
 
-         console.log("Connected correctly to server");
+        console.log("Connected correctly to server");
 
-         const db = client.db(dbName);
+        await insert("I am anxious", 23, false, null);
 
-         // Use the collection "stories"
+        await read("anxiety");
 
-         const col = db.collection("stories");
+    } catch (err) {
 
-         // Construct a document
-         
-         let storyDocument = {
+        console.log(err.stack);
 
-             "text": "I am struggling today",
+    }
 
-             "likes": 0,
-
-             "contact": true,
-
-             "phone_number": 4071234567
-
-         }
-
-         // Insert a single document, wait for promise so we can read it back
-
-         const p = await col.insertOne(storyDocument);
-
-         // Find one document
-
-         const myDoc = await col.findOne();
-
-         // Print to the console
-
-         console.log(myDoc);
-
-        } catch (err) {
-
-         console.log(err.stack);
-
-     }
-
- 
-
-     finally {
+    finally {
 
         await client.close();
 
     }
-
 }
 
-run().catch(console.dir);
+async function insert(text, likes, tags, contact, phone_number) {
+
+    // Construct a document
+    let storyDocument = {
+
+        "text": text,
+        "likes": likes,
+        "tags": ["anxiety", "depression"],
+        "contact": contact,
+        "phone_number": phone_number
+
+    }
+
+    // Insert a single document
+    const p = col.insertOne(storyDocument);
+
+    return new Promise(resolve => {
+        resolve("resolved");
+    });
+}
+
+async function read(tag) {
+
+    // query for stories whose "tags" array includes the given tag
+    const query = {
+        tags: tag
+    };
+    // sort in descending (-1) order by likes
+    const sort = { likes: -1 };
+
+    // find stories matching the query
+    const cursor = col.find(query).sort(sort);
+
+    // print the resulting stories
+    await cursor.forEach(console.dir);
+    // Find one document
+    const myDoc = await col.findOne();
+
+    // Print to the console
+    console.log(myDoc);
+
+    return new Promise(resolve => {
+        resolve("resolved");
+    });
+}
+
+connect().catch(console.dir);
