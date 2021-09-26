f19231d6402618766803f35e9b85bca6e11c4d33
timothyoei <timothy.oei@gmail.com>
2021-09-25 22:27:33 -0400

    Wacky phone stuff

--- src/screens/Help.js
+++ src/screens/Help.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import styles from '../styles/base';
 import { Button } from 'react-native-paper';
+import { Call, Text } from 'react-native-openanything'
 
 export default function Help() {
     return (
         <View style={styles.centeredContainer}>
             <Button 
                 icon='message-processing'
-                onPress={()=>{}}
+                onPress={()=>{Text('7863745755', 'Hello from the other side')}}
             >
                 Text
             </Button>
             <Button 
                 icon='phone'
-                onPress={()=>{}}
+                onPress={()=>{Call('3863856408', true)}}
             >
                 Call
             </Button>
